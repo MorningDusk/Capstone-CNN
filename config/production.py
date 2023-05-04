354715dc329271f0122966da185712463ab256ab
조명하 <cmhhm@naver.com>
2023-05-04 22:35:34 +0900

    db

--- config/production.py
+++ config/production.py
@@ -2,6 +2,8 @@ from config.default import *
 from logging.config import dictConfig
 from dotenv import load_dotenv
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     user=os.getenv('DB_USER'),
     pw=os.getenv('DB_PASSWORD'),
