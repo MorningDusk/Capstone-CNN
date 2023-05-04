02530e15a4e5e67b443ee4c98cf686c71a02859e
조명하 <cmhhm@naver.com>
2023-05-04 22:32:47 +0900

    db

--- config/production.py
+++ config/production.py
@@ -1,7 +1,12 @@
 from config.default import *
 from logging.config import dictConfig
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\x1d"3\xd0&\x08\xcb3\x98\xdb\x91\xc5)\xa2?l'
 
