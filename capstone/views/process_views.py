d5e8e517a659236290d36a803aaba450092ffbda
조명하 <cmhhm@naver.com>
2023-05-21 00:41:53 +0900

    result

--- capstone/views/process_views.py
+++ capstone/views/process_views.py
@@ -27,8 +27,10 @@ def is_image_url(url):
 
 def request_process(id):
     data = {'value': id}
-    url = 'http://7d5b-34-143-145-123.ngrok-free.app/process'
+    url = 'http://112a-35-227-28-199.ngrok-free.app/process'
     response = requests.post(url, json=data)
+
+    print(response.json()['result'])
 
     if response.status_code == 200:
         return response.json()['result']
@@ -40,34 +42,39 @@ def request_process(id):
 def img():
     form = ImageForm()
     if request.method == 'POST':
-        file = request.files['image-file']
-        print(type(file))
-        url = request.form['image-url']
-        if file:
-            user_id = session.get('user_id')
-            time_now = datetime.now(pytz.timezone('Asia/Seoul'))
-            image = Image(user=user_id, image=file.read(), date=time_now)
-            db.session.add(image)
-            db.session.commit()
+        if request.form['type'] == '저장':
+            file = request.files['image-file']
+            url = request.form['image-url']
+            if file:
+                user_id = session.get('user_id')
+                time_now = datetime.now(pytz.timezone('Asia/Seoul'))
+                image = Image(user=user_id, image=file.read(), date=time_now)
+                db.session.add(image)
+                db.session.commit()
 
-            return redirect(url_for('main.index'))
-        elif len(url) != 0:
-            if is_image_url(url):
-                response = requests.get(url)
-                if response.status_code == 200:
-                    user_id = session.get('user_id')
-                    time_now = datetime.now(pytz.timezone('Asia/Seoul'))
-                    image = Image(user=user_id, image=response.content, date=time_now)
-                    db.session.add(image)
-                    db.session.commit()
+                flash("파일이 업로드되었습니다.")
+                return render_template('process/img.html', form=form)
+                #return redirect(url_for('main.index'))
+            elif len(url) != 0:
+                if is_image_url(url):
+                    response = requests.get(url)
+                    if response.status_code == 200:
+                        user_id = session.get('user_id')
+                        time_now = datetime.now(pytz.timezone('Asia/Seoul'))
+                        image = Image(user=user_id, image=response.content, date=time_now)
+                        db.session.add(image)
+                        db.session.commit()
 
-                    return redirect(url_for('main.index'))
+                        flash("파일이 업로드되었습니다.")
+                        return render_template('process/img.html', form=form)
+                        #return redirect(url_for('main.index'))
+                    else:
+                        flash("오류가 발생했습니다.")
                 else:
-                    flash("오류가 발생했습니다.")
+                    flash("올바른 url을 입력해주세요.")
             else:
-                flash("올바른 url을 입력해주세요.")
-        else:
-            flash("파일을 업로드해주세요.")
+                flash("파일을 업로드해주세요.")
+
     return render_template('process/img.html', form=form)
 
 @bp.route('/cam', methods=('GET', 'POST'))
@@ -84,9 +91,16 @@ def cam():
 
     return render_template('process/cam.html')
 
-@bp.route('/result', methods=('GET', 'POST'))
-def result():
-    return render_template('process/result.html')
+@bp.route('/result/<id>', methods=('GET', 'POST'))
+def result(id):
+    id = request_process(id)
+
+    image = Image.query.filter_by(id=id).first()
+    image.image = base64.b64encode(image.image).decode('utf-8')
+    if id != -1:
+        return render_template('process/result.html', image=image)
+    else:
+        flash("이미지 분석에 실패했습니다.")
 
 @bp.route('/captured', methods=('GET', 'POST'))
 def captured():
